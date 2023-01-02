591777387adfbb9d6c354d150a1e5760662de92c
ikustow <ikustow@yandex.ru>
2023-01-02 18:12:26 +0300

    auth

--- services/supabase.service.js
+++ services/supabase.service.js
@@ -1,7 +1,8 @@
 const { createClient } = require('@supabase/supabase-js')
+require('dotenv').config();
 
-const supabaseUrl = 'https://bluchzwajzpsbbcdyrxg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsdWNoendhanpwc2JiY2R5cnhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTgwMDU5NDYsImV4cCI6MTk3MzU4MTk0Nn0.ti0V1Nf8CDNK2Y9wtgMzwCxp2EzStpPs9rQBVz85OZY'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 
 const supabase = createClient(
     supabaseUrl,
